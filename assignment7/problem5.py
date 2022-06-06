da907fa0e8c4a98d4e63a1ec30fc39fbd9181b45
Hiroki-Miyasaka <h.m.jojo.8258@gmail.com>
2022-06-05 18:29:55 -0700

    modified assignment7/problem5.py

---
+++ assignment7/problem5.py
@@ -0,0 +1,34 @@
+# import math
+
+
+# pointList = ["x", "y", "z"]
+# point1 = [1, 4, 1]
+# point2 = [3, 7, 10]
+# point3 = [-1, 3, 4]
+# point4 = [4, 4, 2]
+# point5 = [7, -4, 0]
+# point6 = [10, 4, 1]
+
+# def testFindClosetPointCenter(pointList,):
+    
+
+
+# def createDict(list1, list2):
+#     newList = dict(zip(list1, list2))
+#     return newList
+
+# def calculateDistance(list):
+#     minDistance = 0
+#     tempDistance = 0
+#     for i in range(len(list)):
+#         for j in i.values():
+            
+
+#     return math.sqrt(math.pow(list[0], 2) + math.pow(list[1], 2) + math.pow(list[2], 2))
+
+# def compareDistance(m, n):
+#     if m > n:
+#         return m
+#     return n
+
+
